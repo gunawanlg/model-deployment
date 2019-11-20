d26a063e867ac3267d688ed160c2d705a6690333
Gunawan Gaol (ID) <gunawan.gaol@homecredit.co.id>
2019-11-20 13:13:59 +0700

    try geJSON

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import requests
 
 if __name__ == "__main__":
-    URL = "http://gunawangaol.pythonanywhere.com/"
+    feature1 = str(1.0)
+    feature2 = str(2.0)
+    feature3 = str(3.0)
+    feature4 = str(4.0)
+    feature5 = str(5.0)
     
     datum_body = {
         "body": [
@@ -22,8 +26,11 @@ if __name__ == "__main__":
         ]
     }
 
+    URL = "http://gunawangaol.pythonanywhere.com/"
+    URL2 = "https://gunawangaol.pythonanywhere.com/prediction?feature1="+feature1+"&feature2="+feature2+"&feature3="+feature3+"&feature4="+feature4+"&feature5="+feature5
+
     # sending get request and saving the response as response object 
-    r = requests.get(url = URL, json=datum_body) 
-    
+    # r = requests.get(url = URL, json=datum_body) 
+    r = requests.get(url = URL2)
     # extracting data in json format  
     print(r.json())
